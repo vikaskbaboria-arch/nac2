e63d8661177e55553c59afc4b4c038a4ed40c4a2
vikaskbaboria-arch <vikaskbaboria@gmail.com>
2025-12-30 16:43:10 +0530

    Update page.jsx

--- app/[username]/page.jsx
+++ app/[username]/page.jsx
@@ -2,10 +2,6 @@ import React from 'react'
 import Profile from '@/components/Profile'
 const profile = ({params}) => {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 74579fbfe763d20a75ce6e2643f55bf8856785d0
   return (
     
 <>
