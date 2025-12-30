66141051f4fe2c39e50f04d0e92d0c27f2e88c33
vikaskbaboria-arch <vikaskbaboria@gmail.com>
2025-12-30 16:21:06 +0530

    Update movie.js

--- components/movie.js
+++ components/movie.js
@@ -113,17 +113,10 @@ console.log(movie)
         onClick={() => handleLink(m)}
         className="
           snap-center flex-shrink-0
-<<<<<<< HEAD
           h-38 rounded-md relative
           transition-transform duration-300 hover:scale-105
           cursor-pointer
           w-24 sm:w-40
-=======
-          h-40 rounded-md relative
-          transition-transform duration-300 hover:scale-105
-          cursor-pointer
-          w-28 sm:w-36
->>>>>>> 74579fbfe763d20a75ce6e2643f55bf8856785d0
           sm:h-60
         "
       >
@@ -224,3 +217,4 @@ export default Movie
 
 
 // export default Movie;
+
